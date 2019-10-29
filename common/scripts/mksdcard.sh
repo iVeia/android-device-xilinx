2bde727f5f5815504ecc91307e23cdc0d1bf2480
Brian Silverman <bsilverman@iveia.com>
2019-10-29 10:41:03 -0400

    mksdcard script now includes progress echoes for Qt

--- common/scripts/mksdcard.sh
+++ common/scripts/mksdcard.sh
@@ -65,7 +65,6 @@ fi
 echo "reasonable disk $diskname, partitions ${diskname}${prefix}1..." ;
 
 umount ${diskname}${prefix}*
-
 echo "========= creating partition table"
 parted -s ${diskname} mklabel msdos
 
@@ -74,177 +73,207 @@ echo "========= creating BOOT partition : 2GiB"
 parted -s --align=optimal ${diskname} mkpart primary ${offset}MiB $((offset+2048))MiB
 let offset=offset+2048
 
+echo "<<iVeia_recovery:update:part,.1>>"
 echo "========= creating Extended partition : 13 GiB"
 parted -s --align=optimal ${diskname} mkpart extended ${offset}MiB $((offset+1024*13))MiB
 let offset=offset+4 # Only increment by 4 because we are partitioning inside the extended
 
-echo "********* creating container A ****************"
+echo "<<iVeia_recovery:update:part,.15>>"
+echo "========= creating BootInfo partition : 256 MiB"
+parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+256))MiB
+let offset=offset+512
 echo "========= creating BootInfo partition : 256 MiB"
 parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+256))MiB
 let offset=offset+512
 
+echo "<<iVeia_recovery:update:part,.2>>"
+echo "========= creating ROOT partition : 1GiB"
+parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+1024))MiB
+let offset=offset+1025
 echo "========= creating ROOT partition : 1GiB"
 parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+1024))MiB
 let offset=offset+1025
 
+echo "<<iVeia_recovery:update:part,.25>>"
+echo "========= creating SYS partition : 4 GiB"
+parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+4*1024))MiB
+let offset=offset+4*1024+1
 echo "========= creating SYS partition : 4 GiB"
 parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+4*1024))MiB
 let offset=offset+4*1024+1
 
+echo "<<iVeia_recovery:update:part,.3>>"
 echo "========= creating CACHE partition : 768 MiB"
 parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+768))MiB
 let offset=offset+778
-
-echo "********* creating container B ****************"
-echo "========= creating BootInfo partition : 256 MiB"
-parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+256))MiB
-let offset=offset+512
-
-echo "========= creating ROOT partition : 1GiB"
-parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+1024))MiB
-let offset=offset+1025
-
-echo "========= creating SYS partition : 4 GiB"
-parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+4*1024))MiB
-let offset=offset+4*1024+1
-
 echo "========= creating CACHE partition : 768 MiB"
 parted -s --align=optimal ${diskname} mkpart logical ${offset}MiB $((offset+768))MiB
 let offset=offset+768
 
+echo "<<iVeia_recovery:update:part,.35>>"
 offset=$((2048+1024*13+128))
 echo "========= creating Data partition : 8 GiB"
 parted -s --align=optimal ${diskname} mkpart primary ${offset}MiB $((offset+8*1024))MiB
 let offset=offset+8*1024
 
+echo "<<iVeia_recovery:update:part,.4>>"
 echo "========= creating Scratch partition"
 parted -s --align=optimal ${diskname} mkpart primary ${offset}MiB 100%
 
 sync
 sleep 1
 
+echo "<<iVeia_recovery:update:part,.5>>"
 for n in `seq 1 12` ; do
 	if ! [ -e ${diskname}${prefix}$n ] ; then
 		echo "!!! Error: missing partition ${diskname}${prefix}$n" ;
+		echo "<<iVeia_recovery:complete:part,false>>"
 		exit 1;
 	fi
 	sync
 done
 
+echo "<<iVeia_recovery:update:part,.55>>"
 echo "========= formating BOOT partition"
 mkfs.vfat -F 32 -n BOOT ${diskname}${prefix}1
 
+echo "<<iVeia_recovery:update:part,.6>>"
 echo "========= formating BootInfoA partition"
 mkfs.ext4 -F -L BIA ${diskname}${prefix}5
-
-echo "========= formating ROOTA partition"
-mkfs.ext4 -F -L ROOTA ${diskname}${prefix}6
-
-echo "========= formating SYSTEMA partition"
-mkfs.ext4 -F -L SYSTEMA ${diskname}${prefix}7
-
-echo "========= formating CACHEA partition"
-mkfs.ext4 -F -L CACHEA ${diskname}${prefix}8
-
 echo "========= formating BootInfoB partition"
 mkfs.ext4 -F -L BIB ${diskname}${prefix}9
 
+echo "<<iVeia_recovery:update:part,.65>>"
+echo "========= formating ROOTA partition"
+mkfs.ext4 -F -L ROOTA ${diskname}${prefix}6
 echo "========= formating ROOTB partition"
 mkfs.ext4 -F -L ROOTB ${diskname}${prefix}10
 
+echo "<<iVeia_recovery:update:part,.7>>"
+echo "========= formating SYSTEMA partition"
+mkfs.ext4 -F -L SYSTEMA ${diskname}${prefix}7
 echo "========= formating SYSTEMB partition"
 mkfs.ext4 -F -L SYSTEMB ${diskname}${prefix}11
 
+echo "<<iVeia_recovery:update:part,.75>>"
+echo "========= formating CACHEA partition"
+mkfs.ext4 -F -L CACHEA ${diskname}${prefix}8
 echo "========= formating CACHEB partition"
 mkfs.ext4 -F -L CACHEB ${diskname}${prefix}12
 
+echo "<<iVeia_recovery:update:part,.8>>"
 echo "========= formating DATA partition"
 mkfs.ext4 -F -L DATA ${diskname}${prefix}3
 
+echo "<<iVeia_recovery:update:part,.9>>"
 echo "========= formating scratch partition"
 mkfs.ext4 -F -L Scratch ${diskname}${prefix}4
 
+echo "<<iVeia_recovery:complete:part,true>>"
 
+echo "<<iVeia_recovery:update:pboot,.1>>"
 echo "========= populating BOOT partition"
 if [ -e ${diskname}${prefix}1 ]; then
 	mkdir -p /tmp/boot_part
 	mount -t vfat ${diskname}${prefix}1 /tmp/boot_part
+	echo "<<iVeia_recovery:update:pboot,.2>>"
 	cp -rfv /root/boot/* /tmp/boot_part/
 	sync
 	umount /tmp/boot_part
 	rm -rf /tmp/boot_part
 else
    echo "!!! Error: missing BOOT partition ${diskname}${prefix}1";
+   echo "<<iVeia_recovery:complete:pboot,false>>"
    exit 1
 fi
 
+echo "<<iVeia_recovery:update:pboot,.4>>"
 echo "========= populating BootInfoA partition"
 if [ -e ${diskname}${prefix}5 ]; then
 	mkdir -p /tmp/bi_part
 	mount -t ext4 ${diskname}${prefix}5 /tmp/bi_part
+	echo "<<iVeia_recovery:update:pboot,.5>>"
 	cp -rfv /root/bi/* /tmp/bi_part/
 	sync
 	umount /tmp/bi_part
 	rm -rf /tmp/bi_part
 else
    echo "!!! Error: missing BOOT partition ${diskname}${prefix}1";
+   echo "<<iVeia_recovery:complete:pboot,false>>"
    exit 1
 fi
 
+echo "<<iVeia_recovery:update:pboot,.7>>"
 echo "========= populating BootInfoB partition"
 if [ -e ${diskname}${prefix}9 ]; then
 	mkdir -p /tmp/bi_part
 	mount -t ext4 ${diskname}${prefix}9 /tmp/bi_part
+	echo "<<iVeia_recovery:update:pboot,.8>>"
 	cp -rfv /root/bi/* /tmp/bi_part/
 	sync
 	umount /tmp/bi_part
 	rm -rf /tmp/bi_part
 else
    echo "!!! Error: missing BOOT partition ${diskname}${prefix}1";
+   echo "<<iVeia_recovery:complete:pboot,false>>"
    exit 1
 fi
+echo "<<iVeia_recovery:complete:pboot,true>>"
 
 
+echo "<<iVeia_recovery:update:proot,.1>>"
 echo "========= populating ROOTA partition"
 if [ -e ${diskname}${prefix}6 ]; then
     mkdir -p /tmp/root_part
 	mount -t ext4 ${diskname}${prefix}6 /tmp/root_part
 	cp -r /root/root/* /tmp/root_part/
+	echo "<<iVeia_recovery:update:proot,.4>>"
         cp /tmp/root_part/fstab.zcu102.a /tmp/root_part/fstab.zcu102
 	sync
 	umount /tmp/root_part
 	rm -rf /tmp/root_part
 else
 	echo "!!! Error: missing ROOT partition ${diskname}${prefix}2";
+	echo "<<iVeia_recovery:complete:proot,false>>"
 	exit 1
 fi
 
+echo "<<iVeia_recovery:update:proot,.6>>"
 echo "========= populating ROOTB partition"
 if [ -e ${diskname}${prefix}10 ]; then
     mkdir -p /tmp/root_part
 	mount -t ext4 ${diskname}${prefix}10 /tmp/root_part
 	cp -r /root/root/* /tmp/root_part/
+	echo "<<iVeia_recovery:update:proot,.8>>"
         cp /tmp/root_part/fstab.zcu102.b /tmp/root_part/fstab.zcu102
 	sync
 	umount /tmp/root_part
 	rm -rf /tmp/root_part
 else
 	echo "!!! Error: missing ROOT partition ${diskname}${prefix}2";
+	echo "<<iVeia_recovery:complete:proot,false>>"
 	exit 1
 fi
+echo "<<iVeia_recovery:complete:proot,true>>"
 
+sys_size=$(stat -c%s /root/system.img)
+echo "<<iVeia_recovery:ddcoming:psystem,$sys_size,0.0,0.5>>"
 echo "========= populating SYSTEMA partition"
 if [ -e ${diskname}${prefix}7 ]; then
-	dd if=/root/system.img of=${diskname}${prefix}7
+	dd if=/root/system.img of=${diskname}${prefix}7 status=progress
 else
 	echo "!!! Error: missing SYSTEM partition ${diskname}${prefix}5";
+	echo "<<iVeia_recovery:complete:psystem,false>>"
 	exit 1
 fi
 
+echo "<<iVeia_recovery:ddcoming:psystem,$sys_size,0.5,0.95>>"
 echo "========= populating SYSTEMB partition"
 if [ -e ${diskname}${prefix}11 ]; then
-	dd if=/root/system.img of=${diskname}${prefix}11
+	dd if=/root/system.img of=${diskname}${prefix}11 status=progress
 else
 	echo "!!! Error: missing SYSTEM partition ${diskname}${prefix}5";
+	echo "<<iVeia_recovery:complete:psystem,false>>"
 	exit 1
 fi
+echo "<<iVeia_recovery:complete:psystem,true>>"
